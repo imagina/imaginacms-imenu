568aba56552b3e80f0a90fc258f316105a8dd5c8
msolanogithub <msolano2505@gmail.com>
2025-10-15 12:22:59 -0500

    refactor | update permissions to remove super-admin restrictions

--- config/permissions.php
+++ config/permissions.php
@@ -5,7 +5,7 @@ return [
         'manage' => [
             'title' =>  'imenu::menus.manage.title',
             'description' => 'imenu::menus.manage.description',
-            'onlyFor' => ['super-admin'],
+            'onlyFor' => [],
             'defaultAccess' => []
         ],
         'index' => [
@@ -17,25 +17,25 @@ return [
         'create' => [
             'title' =>  'imenu::menus.create.title',
             'description' => 'imenu::menus.create.description',
-            'onlyFor' => ['super-admin'],
+            'onlyFor' => [],
             'defaultAccess' => []
         ],
         'edit' => [
             'title' =>  'imenu::menus.edit.title',
             'description' => 'imenu::menus.edit.description',
-            'onlyFor' => ['super-admin'],
+            'onlyFor' => [],
             'defaultAccess' => []
         ],
         'destroy' => [
             'title' =>  'imenu::menus.destroy.title',
             'description' => 'imenu::menus.destroy.description',
-            'onlyFor' => ['super-admin'],
+            'onlyFor' => [],
             'defaultAccess' => []
         ],
         'restore' => [
             'title' =>  'imenu::menus.restore.title',
             'description' => 'imenu::menus.restore.description',
-            'onlyFor' => ['super-admin'],
+            'onlyFor' => [],
             'defaultAccess' => []
         ]
     ],
@@ -43,7 +43,7 @@ return [
         'manage' => [
             'title' =>  'imenu::menuitems.manage.title',
             'description' => 'imenu::menuitems.manage.description',
-            'onlyFor' => ['super-admin'],
+            'onlyFor' => [],
             'defaultAccess' => []
         ],
         'index' => [
@@ -55,25 +55,25 @@ return [
         'create' => [
             'title' =>  'imenu::menuitems.create.title',
             'description' => 'imenu::menuitems.create.description',
-            'onlyFor' => ['super-admin'],
+            'onlyFor' => [],
             'defaultAccess' => []
         ],
         'edit' => [
             'title' =>  'imenu::menuitems.edit.title',
             'description' => 'imenu::menuitems.edit.description',
-            'onlyFor' => ['super-admin'],
+            'onlyFor' => [],
             'defaultAccess' => []
         ],
         'destroy' => [
             'title' =>  'imenu::slides.destroy.title',
             'description' => 'imenu::slides.destroy.description',
-            'onlyFor' => ['super-admin'],
+            'onlyFor' => [],
             'defaultAccess' => []
         ],
         'restore' => [
             'title' =>  'imenu::menuitems.restore.title',
             'description' => 'imenu::menuitems.restore.description',
-            'onlyFor' => ['super-admin'],
+            'onlyFor' => [],
             'defaultAccess' => []
         ]
     ]
